7f3059748a15556f67c379cecb690b1d3a5fa275
Andditafsal <anditasalsabila12@gmail.com>
2023-03-12 22:14:54 +0700

    setup routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
 import './bootstrap';
 import router from './router';
+import store from './store';
 
 import  App  from "./core/App.vue";
 import  Auth from "./core/Auth.vue";
 
 
 createApp(App).mount("#app");
-createApp(Auth).use(router).mount("#auth");
+createApp(Auth).use(router).use(store).mount("#auth");
 
 
 
---
+++ resources/js/store/index.js
@@ -0,0 +1,11 @@
+
+import { createStore } from "vuex";
+import action  from "./services/action";
+
+const store = createStore({
+    modules: {
+        action,
+    },
+});
+
+export default store;
