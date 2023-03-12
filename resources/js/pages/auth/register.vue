7607a182ed85237af3951fbe6628c13feb6cb859
Andditafsal <anditasalsabila12@gmail.com>
2023-03-12 22:15:21 +0700

    fetching register

--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -1,3 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                name: "",
+                email: "",
+                password: "",
+            },
+            error: {},
+        };
+    },
+    methods: {
+        handleSubmit() {
+            this.$store
+                .dispatch("postData", ["auth/register", this.form])
+                .then((result) => {
+                    this.$router.push("/auth/login");
+                })
+                .catch((error) => {
+                    this.error = error.response.data.messages;
+                });
+        },
+    },
+};
+</script>
+
 <template>
     <div class="container">
         <div class="card o-hidden border-0 col-lg-7 shadow-lg my-5 mx-auto">
@@ -11,57 +38,62 @@
                                     Register Acount
                                 </h1>
                             </div>
-                            <form class="user">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-user"
-                                            id="exampleFirstName"
-                                            placeholder="First Name"
-                                        />
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-user"
-                                            id="exampleLastName"
-                                            placeholder="Last Name"
-                                        />
+                            <form @submit.prevent="handleSubmit" class="user">
+                                <div class="form-group">
+                                    <input
+                                        type="teks"
+                                        class="form-control form-control-user"
+                                        :class="{ 'is-invalid': error.name }"
+                                        placeholder="Full Name"
+                                        v-model="form.name"
+                                    />
+                                    <div
+                                        class="invalid-feedbeck"
+                                        v-for="(erorr, index) in error.name"
+                                        :key="index"
+                                    >
+                                        {{ erorr }}
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <input
                                         type="email"
                                         class="form-control form-control-user"
-                                        id="exampleInputEmail"
+                                        :class="{ 'is-invalid': error.email }"
                                         placeholder="Email Address"
+                                        v-model="form.email"
                                     />
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input
-                                            type="password"
-                                            class="form-control form-control-user"
-                                            id="exampleInputPassword"
-                                            placeholder="Password"
-                                        />
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input
-                                            type="password"
-                                            class="form-control form-control-user"
-                                            id="exampleRepeatPassword"
-                                            placeholder="Repeat Password"
-                                        />
+                                    <div
+                                        class="invalid-feedbeck"
+                                        v-for="(erorr, index) in error.email"
+                                        :key="index"
+                                    >
+                                        {{ erorr }}
                                     </div>
                                 </div>
-                                <a
-                                    href="login.html"
+                                <div class="form-group">
+                                    <input
+                                        type="password"
+                                        class="form-control form-control-user"
+                                        :class="{
+                                            'is-invalid': error.password,
+                                        }"
+                                        placeholder="password"
+                                        v-model="form.password"
+                                    />
+                                    <div
+                                        class="invalid-feedbeck"
+                                        v-for="(erorr, index) in error.password"
+                                        :key="index"
+                                    >
+                                        {{ erorr }}
+                                    </div>
+                                </div>
+                                <button
                                     class="btn btn-primary btn-user btn-block"
                                 >
                                     Register Account
-                                </a>
+                                </button>
                             </form>
                             <hr />
 
